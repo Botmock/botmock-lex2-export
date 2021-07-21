da5026a469e0798d4912fe27b9243b4c87dfca86
RAHUL <rahul.chauhan049@gmail.com>
2021-07-22 01:00:21 +0530

    Added node files

---
+++ index.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import { Lex2Exporter, FileWriter, Kind, ProjectReference } from "@botmock/export";
+
+/**
+ * Generates `Lex 2 files and folder` in `./output`.
+ *
+ * @example
+ * ```shell
+ * npm start
+ * ```
+ */
+async function main(): Promise<void> {
+  const projectReference: ProjectReference = {
+    teamId: process.env.BOTMOCK_TEAM_ID as string,
+    projectId: process.env.BOTMOCK_PROJECT_ID as string,
+    boardId: process.env.BOTMOCK_BOARD_ID,
+  };
+  const exporter = new Lex2Exporter({ token: env.BOTMOCK_TOKEN as string });
+  const { data } = await exporter.exportProjectUnderDataTransformations({ projectReference });
+
+  const writeResult = await (new FileWriter({ directoryRoot: "./output" })).writeAllResourcesToFiles({ data });
+  if (writeResult.kind !== Kind.OK) {
+    console.error(writeResult.value);
+  }
+}
+
+process.on("unhandledRejection", () => { });
+process.on("uncaughtException", () => { });
+
+main().catch((err: Error) => {
+  console.error(err);
+});
